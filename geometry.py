355120633675f1b93e199b5920bfb22e09450e7b
kakov <semendud74@gmail.com>
2024-05-08 03:13:17 +0300

    add ball(bags)

---
+++ geometry.py
@@ -0,0 +1,32 @@
+def solve(a, b, r, floor, roof):
+    d = r**2 - b**2
+    if d < 0:
+        return 0
+    elif d == 0:
+        return a if floor <= a <= roof else 0
+    else:
+        x1 = d**0.5 + a
+        x2 = -(d**0.5) + a
+        if floor <= x1 <= roof:
+            return x1
+        elif floor <= x2 <= roof:
+            return x2
+        else: 
+            return 0
+
+def intersection(a, b, c, x0, y0, r, floor, roof): 
+    if a == 0:
+        x = solve(x0, c - y0, r, floor, roof)
+        if x == 0:
+            return -1, -1
+        else:
+            return x, c          
+    else:
+        y = solve(y0, c - x0, r, floor ,roof)
+        if y == 0:
+            return -1, -1
+        else:
+            return c, y  
+
+
+print(intersection(1, 0, 1, 0.5, 2, 1, 0, 1))
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
+from geometry import intersection
+from math import ceil
 
 
 class GameSession:
-    def __init__(self):
+    def __init__(self, gravity, images):
         self.window = None
         self.goal1 = None
         self.goal2 = None
         self.player1 = None
         self.player2 = None
-        self.g = 20
+        self.ball = None
+        self.gravity = gravity
+        self.images = images
 
     def AddWindow(self, window):
         self.window = window
@@ -24,22 +28,65 @@ class GameSession:
 
     def AddPlayer2(self, player):
         self.player2 = player
+    
+    def AddBall(self, ball):
+        self.ball = ball
 
+    def hit(self, player):
+        for CoordX in [player.x + player.width, player.x]:
+            if intersection(1, 0, CoordX,
+                        self.ball.x + self.ball.width/2, self.ball.y + self.ball.height/2, self.ball.rad,
+                        player.y, player.y + player.height) != (-1,-1):
+                if self.ball.vx != 0 and self.ball.motionx != player.motionx != 0:
+                    self.ball.motionx = -self.ball.motionx
+                    self.ball.vx = -self.ball.vx
+                else:
+                    self.ball.vx = player.speed/2
+                    self.ball.motionx = player.motionx
+
+
+    def running(self):
+        clock = pygame.time.Clock()
+
+        running = True
+        
+        while running:
+            self.frame()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.player1.motionx = 1
+                    if event.key == pygame.K_LEFT:
+                        self.player1.motionx = -1
+                    if event.key == pygame.K_UP:
+                        if self.player1.y == self.window.height - self.player1.height:
+                            self.player1.motiony = True
+                            self.player1.vy = 50
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.player1.motionx = 0
+                    if event.key == pygame.K_LEFT:
+                        self.player1.motionx = 0
+
+            pygame.display.update()
+            
+            clock.tick(60)
 
     def frame(self):
-        self.player1.x += self.player1.motionx*5
-        if self.player1.motiony == True:
-            if self.player1.y > self.window.height - self.player1.height:
-                self.player1.motiony = False
-                self.player1.y = 600
-                self.player1.vy = 0
-            self.player1.y -= self.player1.vy/10
-            self.player1.vy -= self.g/10
+        self.hit(self.player1)
+        self.player1.moving((self.window.width, self.window.height), self.gravity)
+        self.player2.moving((self.window.width, self.window.height), self.gravity)
+        self.ball.moving((self.window.width, self.window.height))
         self.window.screen.fill((0, 191, 255))
-        self.window.screen.blit(self.goal1.image, (0, 400))
-        self.window.screen.blit(self.goal2.image, (1180, 400))
-        self.window.screen.blit(self.player1.image, (self.player1.x, self.player1.y))
-        self.window.screen.blit(self.player2.image, (self.player2.x, self.player2.y))
+        self.window.screen.blit(self.images["goal1"], (0, 400))
+        self.window.screen.blit(self.images["goal2"], (1180, 400))
+        self.window.screen.blit(self.images["player1"], (self.player1.x, self.player1.y))
+        self.window.screen.blit(self.images["player2"], (self.player2.x, self.player2.y))
+        self.window.screen.blit(self.images["ball"], (self.ball.x, self.ball.y))
 
 
 class Window:
@@ -50,79 +97,84 @@ class Window:
 
 
 class Goal:
-    def __init__(self, width, height, image):
+    def __init__(self, width, height):
 
-       self.image = image
        self.width = width
        self.height = height
 
 
 class Player:
-    def __init__(self, width, height, image, x, y):
+    def __init__(self, width, height, StartPosX, StartPosY, speed):
 
-       self.image = image
        self.width = width
        self.height = height
-       self.x = x
-       self.y = y
+       self.x = StartPosX
+       self.y = StartPosY
        self.vy = 0
        self.motionx = 0
        self.motiony = False
+       self.speed = speed
+    
+    def moving(self, FieldSize, gravity):
+        if (self.x > 0 or self.motionx != -1) and\
+            (self.x < FieldSize[0] - self.width or self.motionx != 1):
+            self.x += self.motionx*self.speed
+        if self.motiony == True:
+            if self.y > FieldSize[1] - self.height:
+                self.motiony = False
+                self.y = FieldSize[1] - self.height
+                self.vy = 0
+                return
+            self.y -= self.vy
+            self.vy -= gravity
+    
 
+class Ball:
+    def __init__(self, width, height, StartPosX, StartPosY):
+        self.width = width
+        self.height = height
+        self.x = StartPosX
+        self.y = StartPosY
+        self.vy = 0
+        self.vx = 0
+        self.motionx = 0
+        self.motiony = False
+        self.rad = self.width/2
+
+    def moving(self, FieldSize):
+        if self.x < 0 or self.x > FieldSize[0] - self.width:
+            self.vx = -self.vx
+            self.motionx = -self.motionx
+        self.x += self.vx
 
 def main():
-    Player1Image = pygame.transform.scale(pygame.image.load("images/player1.png"), (100, 100))
-    Player2Image = pygame.transform.scale(pygame.image.load("images/player2.png"), (100, 100))
-    Goal1Image = pygame.transform.scale(pygame.image.load("images/goal1.png"), (120, 300))
-    Goal2Image = pygame.transform.scale(pygame.image.load("images/goal2.png"), (120, 300))
+    images = {"player1": pygame.transform.scale(pygame.image.load("images/player1.png"), (100, 100)),
+              "player2": pygame.transform.scale(pygame.image.load("images/player2.png"), (100, 100)),
+              "goal1": pygame.transform.scale(pygame.image.load("images/goal1.png"), (120, 300)),
+              "goal2": pygame.transform.scale(pygame.image.load("images/goal2.png"), (120, 300)),
+              "ball": pygame.transform.scale(pygame.image.load("images/ball.png"), (150, 150))}
 
     pygame.init()
-    clock = pygame.time.Clock()
 
-    game = GameSession()
+    game = GameSession(5, images)
     window = Window(1300, 700)
 
-    goal1 = Goal(120, 300, Goal1Image)
-    goal2 = Goal(60, 150, Goal2Image)
-    player1 = Player(100, 100, Player1Image, 120, 600)
-    player2 = Player(100, 100, Player2Image, 1080, 600)
+    goal1 = Goal(120, 300)
+    goal2 = Goal(60, 150)
+    player1 = Player(100, 100, 120, 600, 5)
+    player2 = Player(100, 100, 1080, 600, 5)
+    ball = Ball(150, 150, 575, 550)
 
     game.AddWindow(window)
     game.AddGoal1(goal1)
     game.AddGoal2(goal2)
     game.AddPlayer1(player1)
     game.AddPlayer2(player2)
-    game.frame()
+    game.AddBall(ball)
 
     pygame.display.update()
 
-    running = True
-    while running:
-        game.window.screen.fill((0, 191, 255))
-        game.frame()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    game.player1.motionx = 1
-                if event.key == pygame.K_LEFT:
-                    game.player1.motionx = -1
-                if event.key == pygame.K_UP:
-                    if game.player1.y == game.window.height - game.player1.height:
-                        game.player1.motiony = True
-                        game.player1.vy = 50
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    game.player1.motionx = 0
-                if event.key == pygame.K_LEFT:
-                    game.player1.motionx = 0
-
-
-
-        pygame.display.update()
-        clock.tick(60)
-
+    game.running()
 
     pygame.quit()
 
